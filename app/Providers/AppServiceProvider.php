198d932624acd8d2072435c1ff8902ad2787579b
sizukutamago <tanaka@tam-bourine.co.jp>
2019-04-06 19:46:06 +0900

    mysqlエラー解消

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Acme\Domain\Repositories\StoreRepositoryInterface;
 use Acme\Infrastructures\Repositories\StoreRepository;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,6 +26,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191);
         $this->app->bind(
             StoreRepositoryInterface::class, StoreRepository::class
         );
